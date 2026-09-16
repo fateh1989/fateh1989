67e673cfd987e1a462c3854f98fc12a8fc26db53
fateh1989 <0046alla@gmail.com>
2026-09-16 06:19:09 +0200

    Bump YM to v0.14 full runtime

--- ym-ci/android/app/build.gradle.kts
+++ ym-ci/android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ym.lite.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.13.0"
+        versionCode = 14
+        versionName = "0.14.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
