733ab4ee20ea458f85fe48e58ee7b53e379f933f
fateh1989 <0046alla@gmail.com>
2026-09-16 05:05:23 +0200

    Bump YM official-controller version

--- ym-ci/android/app/build.gradle.kts
+++ ym-ci/android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ym.lite.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.9.0"
+        versionCode = 10
+        versionName = "0.10.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
