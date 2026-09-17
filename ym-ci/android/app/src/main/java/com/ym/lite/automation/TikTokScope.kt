5cd9a7baf492bb61f2c1de0627b00f87155c863a
fateh1989 <0046alla@gmail.com>
2026-09-17 21:02:50 +0200

    YM v0.30.2: target main TikTok only

--- ym-ci/android/app/src/main/java/com/ym/lite/automation/TikTokScope.kt
+++ ym-ci/android/app/src/main/java/com/ym/lite/automation/TikTokScope.kt
@@ -4,8 +4,6 @@ object TikTokScope {
     val allowedPackages: List<String> = listOf(
         "com.zhiliaoapp.musically",
         "com.ss.android.ugc.trill",
-        "com.tiktok.lite.go",
-        "com.zhiliaoapp.musically.go",
     )
 
     fun isAllowed(packageName: CharSequence?): Boolean {
