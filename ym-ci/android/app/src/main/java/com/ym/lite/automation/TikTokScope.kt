2a7cc860aa3306704dd783cd223c350989f13641
fateh1989 <0046alla@gmail.com>
2026-09-17 08:24:18 +0200

    YM v0.28: restore simple engine with TikTok Lite scope

--- ym-ci/android/app/src/main/java/com/ym/lite/automation/TikTokScope.kt
+++ ym-ci/android/app/src/main/java/com/ym/lite/automation/TikTokScope.kt
@@ -4,6 +4,8 @@ object TikTokScope {
     val allowedPackages: List<String> = listOf(
         "com.zhiliaoapp.musically",
         "com.ss.android.ugc.trill",
+        "com.tiktok.lite.go",
+        "com.zhiliaoapp.musically.go",
     )
 
     fun isAllowed(packageName: CharSequence?): Boolean {
