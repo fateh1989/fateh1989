e1d10f11a1581389ef866dc16774a482889a5abd
fateh1989 <0046alla@gmail.com>
2026-09-17 22:35:25 +0200

    YM 0.35.1: bump auto comment fix version

--- ym-ci/android/app/build.gradle.kts
+++ ym-ci/android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.ym.lite.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 40
-        versionName = "0.35.0"
+        versionCode = 41
+        versionName = "0.35.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "TIKTOK_CLIENT_KEY", "\"${tiktokClientKey.replace("\\", "\\\\").replace("\"", "\\\"")}\"")
         buildConfigField("String", "TIKTOK_REDIRECT_URL", "\"${tiktokRedirectUrl.replace("\\", "\\\\").replace("\"", "\\\"")}\"")
